c89b89a7843984e5922578e5c30a005f388bcedb
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-18 00:57:48 +0100

    Refactor: enhance init_config function and update app.py for better environment variable handling

--- src/app.py
+++ src/app.py
@@ -1,5 +1,12 @@
 # AddiPi Raspberry Pi Printer Agent
 from config.config import init_config
 
+(
+    device_conn_string,
+    storage_conn_string,
+    octoprint_api_key,
+    octoprint_url
+) = init_config()
 
-init_config()
+
+print(octoprint_api_key)
--- src/config/config.py
+++ src/config/config.py
@@ -1,2 +1,27 @@
+import os
+
+
 def init_config():
-    print('config')
+    device_conn_string = os.getenv('DEVICE_CONNECTION_STRING')
+
+    if not device_conn_string:
+        raise ValueError('DEVICE_CONNECTION_STRING is not set')
+
+    storage_conn_string = os.getenv('STORAGE_CONN')
+
+    if not storage_conn_string:
+        raise ValueError('STORAGE_CONN is not set')
+
+    octoprint_api_key = os.getenv('OCTOPRINT_API_KEY')
+
+    if not octoprint_api_key:
+        raise ValueError('OCTOPRINT_API_KEY is not set')
+
+    octoprint_url = os.getenv('OCTOPRINT_URL', 'http://localhost:5000')
+
+    return (
+        device_conn_string,
+        storage_conn_string,
+        octoprint_api_key,
+        octoprint_url
+        )
