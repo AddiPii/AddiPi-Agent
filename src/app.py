9200cc2c68f717c507c0c0dc43394d07659166f4
Oliwer Urbaniak <oliwerx12@gmail.com>
2025-12-18 00:59:27 +0100

    Refactor: move main logic into a dedicated main function in app.py

--- src/app.py
+++ src/app.py
@@ -1,12 +1,17 @@
 # AddiPi Raspberry Pi Printer Agent
 from config.config import init_config
 
-(
-    device_conn_string,
-    storage_conn_string,
-    octoprint_api_key,
-    octoprint_url
-) = init_config()
+
+def main():
+    (
+        device_conn_string,
+        storage_conn_string,
+        octoprint_api_key,
+        octoprint_url
+    ) = init_config()
+
+    print(octoprint_api_key)
 
 
-print(octoprint_api_key)
+if __name__ == '__main__':
+    main()
